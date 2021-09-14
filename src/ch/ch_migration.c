7c909d642abbca1677c5a151ea53477093e72cb4
Wei Liu <liuwe@microsoft.com>
2021-09-14 13:34:26 +0000

    Cookie helpers
    Signed-off-by: Wei Liu <liuwe@microsoft.com>

--- src/ch/ch_migration.c
+++ src/ch/ch_migration.c
@@ -18,3 +18,85 @@
  * <http://www.gnu.org/licenses/>.
  */
 
+#include <config.h>
+
+#include "virlog.h"
+#include "virerror.h"
+#include "viralloc.h"
+
+#include "ch_migration.h"
+
+#define VIR_FROM_THIS VIR_FROM_CH
+
+VIR_LOG_INIT("ch.ch_migration");
+
+typedef struct _chMigrationCookie chMigrationCookie;
+typedef chMigrationCookie *chMigrationCookiePtr;
+struct _chMigrationCookie {
+	/* Nothing for now */
+};
+
+static void
+chMigrationCookieFree(chMigrationCookiePtr mig)
+{
+    if (!mig)
+        return;
+
+    VIR_FREE(mig);
+}
+
+
+static chMigrationCookiePtr
+chMigrationCookieNew(virDomainObjPtr dom)
+{
+    chMigrationCookiePtr mig = NULL;
+
+    if (VIR_ALLOC(mig) < 0)
+        goto error;
+
+    /* Nothing to do */
+    (void)dom;
+
+    return mig;
+
+error:
+    chMigrationCookieFree(mig);
+    return NULL;
+}
+
+static int
+chMigrationBakeCookie(chMigrationCookiePtr mig, char **cookieout,
+                      int *cookieoutlen)
+{
+    g_auto(virBuffer) buf = VIR_BUFFER_INITIALIZER;
+
+    if (!cookieout || !cookieoutlen)
+        return 0;
+
+    /* Nothing to do */
+    (void)mig;
+
+    *cookieout = NULL;
+    *cookieoutlen = 0;
+
+    VIR_DEBUG("cookielen=%d cookie=%s", *cookieoutlen, *cookieout);
+
+    return 0;
+}
+
+static int
+chMigrationEatCookie(const char *cookiein, int cookieinlen,
+                     chMigrationCookiePtr *migout)
+{
+    chMigrationCookiePtr mig = NULL;
+
+    if (VIR_ALLOC(mig) < 0)
+        return -1;
+
+    /* Nothing to do */
+    (void)cookiein;
+    (void)cookieinlen;
+    *migout = mig;
+
+    return 0;
+}
